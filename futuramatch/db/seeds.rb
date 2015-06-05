101959aa62f07fbdf03b980dfae31b7bc97e0edc
NarfOner <srlk_doblev@hotmail.com>
2015-06-05 16:03:05 +0200

    litle css added, seeded with more characters

--- futuramatch/db/seeds.rb
+++ futuramatch/db/seeds.rb
@@ -12,8 +12,8 @@ User.create(name: 'Bender', age: 100, favourite_food: 'Cerveza', fav_prog_langua
 User.create(name: 'Prof. Farnsworth', age: 99, favourite_food: 'Galletas', fav_prog_language: 'Ensamblador', pic: 'http://www.reactiongifs.us/wp-content/uploads/2013/05/live_on_this_planet_futurama.gif')
 User.create(name: 'Mordisquitos', age: 20, favourite_food: 'Galletas', fav_prog_language: 'Java', pic: 'http://www.latostadora.com/generados/tiendas/9642/dibujos/104836.jpg')
 
-User.create(name: 'Amy Wong', age: , favourite_food: 'Anchoas', fav_prog_language: 'PHP', pic: 'http://vignette1.wikia.nocookie.net/en.futurama/images/9/94/AmyWong.png/revision/20110710094318')
-User.create(name: 'Zack Brannigan', age: , favourite_food: 'Galletas', fav_prog_language: 'Java', pic: 'http://upload.wikimedia.org/wikipedia/en/1/15/Captain_Zapp_Brannigan.png')
-User.create(name: 'Dr. Zoidberg', age: , favourite_food: 'Basura', fav_prog_language: 'Ruby', pic: 'http://img3.wikia.nocookie.net/__cb20050828045205/uncyclopedia/images/c/cd/Doctor_Zoidberg.png')
-User.create(name: 'Hermes', age: , favourite_food: 'Pizza', fav_prog_language: 'PHP', pic: 'http://vignette4.wikia.nocookie.net/es.futurama/images/b/bd/197_hermes-conrad.gif/revision/latest?cb=20100811031842')
-User.create(name: 'Mamá', age: , favourite_food: 'Cerveza', fav_prog_language: 'Ensamblador', pic: 'http://vignette3.wikia.nocookie.net/es.futurama/images/d/d9/Mom.png/revision/latest?cb=20100720202748')
+User.create(name: 'Amy Wong', age: 26, favourite_food: 'Anchoas', fav_prog_language: 'PHP', pic: 'http://vignette1.wikia.nocookie.net/en.futurama/images/9/94/AmyWong.png/revision/20110710094318')
+User.create(name: 'Zack Brannigan', age: 37, favourite_food: 'Galletas', fav_prog_language: 'Java', pic: 'http://upload.wikimedia.org/wikipedia/en/1/15/Captain_Zapp_Brannigan.png')
+User.create(name: 'Dr. Zoidberg', age: 50, favourite_food: 'Basura', fav_prog_language: 'Ruby', pic: 'http://img3.wikia.nocookie.net/__cb20050828045205/uncyclopedia/images/c/cd/Doctor_Zoidberg.png')
+User.create(name: 'Hermes', age: 40, favourite_food: 'Pizza', fav_prog_language: 'PHP', pic: 'http://vignette4.wikia.nocookie.net/es.futurama/images/b/bd/197_hermes-conrad.gif/revision/latest?cb=20100811031842')
+User.create(name: 'Mamá', age: 99, favourite_food: 'Cerveza', fav_prog_language: 'Ensamblador', pic: 'http://vignette3.wikia.nocookie.net/es.futurama/images/d/d9/Mom.png/revision/latest?cb=20100720202748')
